ea25a400a492a14b933ba61831091456a583d1fa
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-27 19:43:05 -0500

    major changes

--- PokeDiscovery/PokeAnnotation.swift
+++ PokeDiscovery/PokeAnnotation.swift
@@ -167,12 +167,14 @@ class PokeAnnotation: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
     var pokemonNumber: Int
     var pokemonName: String
+    var title: String?
     
     init(coordinate: CLLocationCoordinate2D, pokemonNumber: Int) {
         
         self.coordinate = coordinate
         self.pokemonNumber = pokemonNumber
         self.pokemonName = pokemon[pokemonNumber - 1].capitalized
+        self.title = self.pokemonName
     }
 }
 
